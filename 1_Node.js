dd515ae1d265902da4dc1e3494d15d0ad99dd2b6
KortumLu <xiaoweilulu919@hotmail.com>
2021-10-15 15:35:39 +0200

    Create 1_Node.js

---
+++ 1_Node.js
@@ -0,0 +1,56 @@
+Node.js
+
+Node.js不是一种语言，是使用JavaScript进行服务器端应用程序开发的运行时环境
+Node.js 可提供实现用JavaScript编写的程序所需的一切
+Node.js使JavaScript可以独立运行在机器上，从而扩大了JavaScript的范围
+类似于Python语言，可以做各种事情
+
+与PHP的区别：
+PHP是一种语言，而Node.js是一种框架
+PHP和Node.js都可以管理任何复杂的Web应用程序，但是它们是用不同的体系结构和概念构建的。
+
+如果想要良好的数据库集成，那么PHP是理想选择
+如果要设计交互式用户界面，则应该使用Node.js
+
+PHP：
+-- 适用于WAMP，MAMP和LAMP等软件堆栈
+-- 适用于Drupal，WordPress，Joomla等内容管理系统
+-- 适用于SQL，MySQL，Oracle等服务器
+
+Node.js：
+
+-- 单页动态Web应用程序（SPA）。
+-- 与软件堆栈（如MEAN堆栈）一起使用。
+-- 使用服务器端技术，例如Express.js，Node.JS，MongoDB等。
+-- 与Angular.JS，jQuery，React.JS等前端技术一起使用。 
+
+与Node.js中的编程相比，PHP中的编程要容易得多。
+PHP中的编程风格更简单，不需要任何编译器或转换器，也没有托管限制，并且在PHP中使用SQL进行整个设置非常简单
+当遇到诸如正常数据提取要求之类的小用例时，PHP可以快速启动并在短时间内使应用程序运行
+
+而Node.js非常复杂
+在Node.js中部署框架和应用程序并不容易，并且需要精心准备好服务器基础架构
+PHP可与MySQL数据库完美配合，同时为PostgreSQL和MariaDB等多种数据库形式提供类似的支持
+Node.js包含用于访问SQL的各种库。此外，它使用JSON附加到NoSQL数据库
+也可以在JSON的帮助下配置PHP，但是最好使用Node，因为对于服务器和浏览器来说只有一个代码
+
+PHP几乎可以在任何要运行Node的服务器上运行
+要运行Node，需要一台具有ssh访问权限的虚拟机
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
